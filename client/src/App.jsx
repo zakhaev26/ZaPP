c2acc658434a37e467937d265c5d8df6801bf199
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-08 22:42:15 +0530

    added dependencies and folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+function App() {
+
+  return (
+    <>
+    <Button colorScheme='blue'>Upload</Button>
+    <Button colorScheme='teal' size='lg'>
+    Button
+  </Button>
+     <h1>Hello!!</h1>
+    </>
+  )
+}
+
+export default App
