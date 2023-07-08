8749bff14faa3ca7779de7b5a6cac673edf31e69
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-09 00:02:23 +0530

    added server proxy and cors
    linked client with server

--- client/src/Pages/Chatpage.jsx
+++ client/src/Pages/Chatpage.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import axios from "axios"
 function Chatpage() {
+
+  const fetchData = async () => {
+    const data = await axios.get("http://localhost:8089/api/data");
+    console.log("data = " + JSON.stringify(data.data) );
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div>
       <h1>Chatpage</h1>
--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
+app.use(cors());
+app.get('/api/data',(req,res)=>{
+    console.log('requested resource by user!');
+    res.status(200).send('Hi Mom,This is Backend!');
+})
+
 
 app.listen(process.env.PORT || 4000,()=>{
     console.log('Server live at %d',process.env.PORT)
