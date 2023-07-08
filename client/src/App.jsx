538c56744bae1a11fd504f5cfb751b0a91abb4de
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-08 23:48:46 +0530

    Set Pages routes using React-Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Chatpage from './Pages/Chatpage.jsx';
+import Homepage from './Pages/Homepage.jsx';
 function App() {
 
   return (
-    <>
-    <Button colorScheme='blue'>Upload</Button>
-    <Button colorScheme='teal' size='lg'>
-    Button
-  </Button>
-     <h1>Hello!!</h1>
-    </>
+    <div className='App'>
+     <Route exact path='/' component = {Homepage} />
+     <Route path='/chats' component = {Chatpage} />
+    </div>
   )
 }
 
---
+++ client/src/Pages/Chatpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chatpage() {
+  return (
+    <div>
+      <h1>Chatpage</h1>
+    </div>
+  )
+}
+
+export default Chatpage
