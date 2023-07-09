020066dce3e9e85bb2602beddfdc5adc1d982238
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-09 07:20:22 +0530

    switched from cloud mongo to local db
    error while connecting to cloud MongoDB due to network issues

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./db/connect.js"
+
+
 import cors from "cors";
 app.use(cors());
 app.get('/api/data',(req,res)=>{
@@ -9,6 +12,7 @@ app.get('/api/data',(req,res)=>{
     res.status(200).send('Hi Mom,This is Backend!');
 })
 
+connectDB();
 
 app.listen(process.env.PORT || 4000,()=>{
     console.log('Server live at %d',process.env.PORT)
