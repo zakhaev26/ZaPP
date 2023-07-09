f99e6118b5546862dda4ee6806566db474090480
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-09 11:56:44 +0530

    auth functionality complete

--- server/server.js
+++ server/server.js
@@ -5,12 +5,16 @@ import cors from "cors";
 import userRoutes from "./routes/userRoutes.js"
 dotenv.config();
 import connectDB from "./db/connect.js"
+import { notFound,errorHandler } from "./middlewares/errorMiddleware.js";
 connectDB();
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.use(cors());
 app.use(express.json());//to accept JSON Data
 app.use('/api/user',userRoutes);
+
 
 
 app.listen(process.env.PORT || 4000,()=>{
